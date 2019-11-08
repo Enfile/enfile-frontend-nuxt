d1f2d5bfcb79fb59cf7949b5cb3185f3e28eb43e
mogumannto <mogus.workshop@gmail.com>
2019-11-08 16:19:17 +0900

    デフォルトのデザイン修正
    デフォルトのデザインがdarkのため、lightに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,6 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -69,7 +68,12 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        }
+        },
+        light: {
+          primary: colors.red.darken1, // #E53935
+          secondary: colors.red.lighten4, // #FFCDD2
+          accent: colors.indigo.base, // #3F51B5
+        },
       }
     }
   },
