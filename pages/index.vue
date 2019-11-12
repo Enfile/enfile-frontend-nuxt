29ff3c1b77a19085452348edbf3e2a93c611e421
mogumannto <mogus.workshop@gmail.com>
2019-11-12 17:27:51 +0900

    ログインページ用のレイアウトを表示するように設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6 />
-  </v-layout>
+  <v-row>
+    <v-col cols="sm-12">
+      <div class="d-inline-block text-center">
+        <h1 class="display-4">
+          Enfile
+        </h1>
+        <p class="headline">
+          エンジニアを繋ぐプロフィール
+        </p>
+      </div>
+      <v-divider />
+    </v-col>
+    <v-col cols="sm-12">
+      <v-btn color="success" class="mt-3 text-center" block>
+SignIn
+</v-btn>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
+  layout: 'signin',
   components: {}
 }
 </script>
+
+<style lang="scss" scoped>
+.display-4 {
+  font-family: 'Josefin Sans', sans-serif !important;
+}
+</style>
