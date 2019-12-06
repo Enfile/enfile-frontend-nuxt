a9126fdc718b170e4cb2f4041b4fdad6582f92f0
mogumannto <mogus.workshop@gmail.com>
2019-12-06 17:49:57 +0900

    タイトルの位置を上下中央に修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,26 @@
 <template>
-  <v-row>
-    <v-col cols="sm-12">
-      <div class="d-inline-block text-center">
-        <h1 class="display-4">
-          Enfile
-        </h1>
-        <p class="headline">
-          エンジニアを繋ぐプロフィール
-        </p>
-      </div>
-      <v-divider />
-    </v-col>
-    <v-col cols="sm-12">
-      <v-btn color="success" class="mt-3 text-center" block>
-        SignIn
-      </v-btn>
-    </v-col>
-  </v-row>
+  <div class="wrap">
+    <v-row>
+      <v-col cols="sm-12">
+        <div class="d-block text-center">
+          <h1 class="display-4">
+            Enfile
+          </h1>
+          <p class="headline">
+            エンジニアを繋ぐプロフィール
+          </p>
+        </div>
+        <v-divider />
+      </v-col>
+      <v-col cols="sm-12" class="text-center">
+        <div class="my-3">
+          <v-btn color="success" min-width="200">
+            SignIn
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -27,6 +31,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrap {
+  height: 90vh;
+  display: flex;
+  justify-content: center; /*左右中央揃え*/
+  align-items: center; /*上下中央揃え*/
+}
 .display-4 {
   font-family: 'Josefin Sans', sans-serif !important;
 }
