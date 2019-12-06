e652dd47cf06d4bc1870474205be849e21601328
mogumannto <mogus.workshop@gmail.com>
2019-12-06 17:27:02 +0900

    改行修正

--- layouts/signin.vue
+++ layouts/signin.vue
@@ -5,10 +5,7 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    <v-footer :fixed="fixed" app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
