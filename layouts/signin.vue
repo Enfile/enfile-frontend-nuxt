1207176abf12861fe4ce797993cee86e11184ad5
mogumannto <mogus.workshop@gmail.com>
2019-12-06 18:02:29 +0900

    fix.code

--- layouts/signin.vue
+++ layouts/signin.vue
@@ -5,14 +5,7 @@
         <nuxt />
       </v-container>
     </v-content>
-<<<<<<< HEAD
     <v-footer :fixed="fixed" app>
-=======
-    <v-footer
-      :fixed="fixed"
-      app
-    >
->>>>>>> e89ac475993bd1db377c9117e5c730196d56ab6d
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
