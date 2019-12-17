f0e1ad21423a75d3a5a918f8843639bdedca1d73
小佐 海斗 <kosakaito@kosamac.local>
2019-12-17 15:44:47 +0900

    liとか消した

--- components/profile/EditBtn.vue
+++ components/profile/EditBtn.vue
@@ -1,16 +1,12 @@
 <template>
-  <div>
-    <ul class="edit">
-      <li>
-        <button @click="edit">
-          <v-btn class="mx-2" fab dark small color="cyan">
-            <v-icon dark>
-              mdi-pencil
-            </v-icon>
-          </v-btn>
-        </button>
-      </li>
-    </ul>
+  <div class="edit">
+    <button @click="edit">
+      <v-btn class="mx-2" fab dark small color="cyan">
+        <v-icon dark>
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+    </button>
   </div>
 </template>
 
