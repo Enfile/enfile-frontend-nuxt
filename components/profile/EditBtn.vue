50fb56143fa3867448f438ebcfb8fd47a3b4007d
小佐 海斗 <kosakaito@kosamac.local>
2019-12-17 15:06:38 +0900

    編集ボタン html終了

---
+++ components/profile/EditBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul class="edit">
+      <li>
+        <button @click="edit">
+          <v-btn class="mx-2" fab dark small color="cyan">
+            <v-icon dark>
+              mdi-pencil
+            </v-icon>
+          </v-btn>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    edit: function() {
+      this.edit_fig = true
+    }
+  }
+}
+</script>
