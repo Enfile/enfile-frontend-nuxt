5436e16609cce7163ed36ee27d4832c2af1a7e0f
mogumannto <mogus.workshop@gmail.com>
2020-01-21 05:10:08 +0900

    Intoroduction.vue html修正

--- components/profile/Introduction.vue
+++ components/profile/Introduction.vue
@@ -1,117 +1,172 @@
 <template>
-  <v-card class="mx-auto" max-width="auto">
-    <v-card-text>
-      <p class="display-1 text--primary">
+  <v-card class="mx-auto my-3" max-width="auto">
+    <v-card class="mx-auto" max-width="auto" outlined>
+      <v-card-title primary-title>
         自己紹介
-      </p>
-      <div class="text--primary">
+      </v-card-title>
+      <v-card-text>
         hogehogehogehogehogehogehogehoeghhoehegheogheoheohehgegheoheheogh
-      </div>
-      <p class="display-1 text--primary">
+      </v-card-text>
+
+      <v-divider />
+      <v-card-title primary-title>
         業務経験
-      </p>
-      <div class="text--primary">
-        株式会社×××××
+      </v-card-title>
+      <div>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>株式会社×××××</v-list-item-title>
+            <v-list-item-subtitle>
+              アルバイト
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              使用言語：C言語
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              開発ジャンル：ハード
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              2019/01 - 2019/10
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </div>
-      <div class="overline mb-4">
-        アルバイト
-      </div>
-      <div class="text--primary">
-        使用言語：C言語<br>
-        開発ジャンル：：ハード<br>
-        2019/01 - 2019/10
-      </div>
-      <div class="text--primary">
-        株式会社×××××
-      </div>
-      <div class="overline mb-4">
-        アルバイト
-      </div>
-      <div class="text--primary">
-        使用言語：C言語<br>
-        開発ジャンル：：ハード<br>
-        2019/01 - 2019/10
-      </div>
-      <p class="display-1 text--primary">
+
+      <v-divider />
+      <v-card-title primary-title>
         成果物
-      </p>
-      <div class="text--primary">
-        hogeシステム
+      </v-card-title>
+      <div>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>hogeシステム</v-list-item-title>
+            <v-list-item-subtitle>
+              個人
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              使用言語：C言語
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              開発ジャンル：ハード
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              2019/01 - 2019/10
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </div>
-      <div class="overline mb-4">
-        個人
-      </div>
-      <div class="text--primary">
-        使用言語：C言語<br>
-        開発ジャンル：：ハード<br>
-        2019/01 - 2019/10
-      </div>
-      <div class="text--primary">
-        piyopiyo
-      </div>
-      <div class="overline mb-4">
-        チーム
-      </div>
-      <div class="text--primary">
-        使用言語：PHP<br>
-        開発ジャンル：：web<br>
-        2019/01 - 2019/10
-      </div>
-      <p class="display-1 text--primary">
+
+      <v-divider />
+      <v-card-title primary-title>
         使ったことある技術
-      </p>
-      <div class="text--primary">
-        C言語
+      </v-card-title>
+      <div>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>C言語</v-list-item-title>
+            <v-list-item-subtitle>
+              レベル：3
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              期間：3年
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              何に使ったか：アルバイトで使用
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </div>
-      <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
-        何に使ったか：アルバイトで使用
-      </div>
-      <div class="text--primary">
-        PHP
-      </div>
-      <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
-        何に使ったか：個人での個人でのwebアプリ開発で使用
-      </div>
-      <p class="display-1 text--primary">
+
+      <v-divider />
+      <v-card-title primary-title>
         使いたくない技術
-      </p>
-      <div class="text--primary">
-        C言語
+      </v-card-title>
+      <div>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>C言語</v-list-item-title>
+            <v-list-item-subtitle>
+              レベル：3
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              期間：3年
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              何に使ったか：アルバイトで使用
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </div>
-      <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
-        何に使ったか：アルバイトで使用
-      </div>
-      <p class="display-1 text--primary">
+
+      <v-divider />
+      <v-card-title primary-title>
         使っているOS
-      </p>
-      <div class="text--primary">
-        C言語
+      </v-card-title>
+      <div>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>C言語</v-list-item-title>
+            <v-list-item-subtitle>
+              レベル：3
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              期間：3年
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              何に使ったか：アルバイトで使用
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </div>
-      <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
-        何に使ったか：アルバイトで使用
+
+      <v-divider />
+      <v-card-title primary-title>
+        使っているOS
+      </v-card-title>
+      <div>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title>C言語</v-list-item-title>
+            <v-list-item-subtitle>
+              レベル：3
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              期間：3年
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              何に使ったか：アルバイトで使用
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </div>
-      <p class="display-1 text--primary">
+
+      <v-divider />
+      <v-card-title primary-title>
         基本プロフィール
-      </p>
-      <div class="text--primary">
-        男性<br>
-        生年月日：1996/01/21
-      </div>
-      <p class="display-1 text--primary">
+      </v-card-title>
+      <v-card-text>
+        <p>男性</p>
+        <p>生年月日：1996/01/21</p>
+      </v-card-text>
+
+      <v-divider />
+      <v-card-title primary-title>
         連絡先
-      </p>
-      <div class="text--primary">
-        ・https://twitter.com/xxxxxxx<br>
-        ・https://twitter.com/yyyyyyy
-      </div>
-    </v-card-text>
+      </v-card-title>
+      <v-card-text>
+        <ul>
+          <li>
+            <a href="https://twitter.com/xxxxxxx">
+              https://twitter.com/xxxxxxx
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/xxxxxxx">
+              https://twitter.com/xxxxxxx
+            </a>
+          </li>
+        </ul>
+      </v-card-text>
+    </v-card>
   </v-card>
 </template>
