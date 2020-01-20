457567336ff6bc4a676c426abfaddc4cbc82ec43
agoetc <b6151@oic.jp>
2020-01-21 07:32:10 +0900

    検索基盤の作成

--- pages/search.vue
+++ pages/search.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
-    <v-card max-width="450" class="mx-auto">
+    <v-form ref="form" v-model="form.valid" lazy-validation>
+      <v-card>
+        <v-card-title
+          class="headline font-weight-regular blue-grey white--text"
+        >
+          基本プロフィール
+        </v-card-title>
+        <v-card-text class="mt-3">
+          <v-text-field v-model="form.name" :counter="20" label="名前" />
+          <v-text-field
+            v-model="form.school_name"
+            :counter="20"
+            label="学校名"
+          />
+          <v-text-field v-model="form.using_os" :counter="20" label="使用OS" />
+          <v-text-field
+            v-model="form.technology_name"
+            :counter="20"
+            label="技術名"
+          />
+
+          <v-radio-group v-model="form.order_by" label="レベル" row>
+            <v-radio :label="`なし`" :value="``" />
+            <v-radio :label="`昇順`" :value="`order_by_level`" />
+            <v-radio :label="`降順`" :value="`-order_by_level`" />
+          </v-radio-group>
+
+          <div class="mt-5">
+            <v-btn class="mr-4" @click="search">
+              検索
+            </v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-form>
+    <v-card max-width="450" class="mx-auto users">
       <v-list three-line>
         <template v-for="user in users">
           <v-list-item :key="user.created_at">
@@ -29,19 +64,38 @@ export default {
     TecLevelBadge
   },
   data: () => ({
-    users: []
+    users: [],
+    form: {
+      name: '',
+      school_name: '',
+      using_os: '',
+      technology_name: '',
+      order_by: ''
+    }
   }),
   async created() {
-    await this.$axios
-      .$get(`/user/`)
-      .then(response => {
-        this.users = response
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    await this.loadUsers('')
+  },
+  methods: {
+    async loadUsers(query) {
+      await this.$axios
+        .$get(`/user/${query}`)
+        .then(response => {
+          this.users = response
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    async search() {
+      this.loadUsers('')
+    }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.users {
+  margin-top: 20px;
+}
+</style>
