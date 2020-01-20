92ecc4f5f42b870192eb3c9f0a71239610c90aca
agoetc <b6151@oic.jp>
2020-01-21 04:34:24 +0900

    壊れたので修正

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>{{ user.profile }}</p>
+    <p>{{ user }}</p>
     <top-icon />
     <introduction />
   </div>
@@ -13,9 +13,47 @@ import TopIcon from '@/components/profile/TopIcon.vue'
 export default {
   layout: 'user-detail',
   components: { Introduction, TopIcon },
-  async asyncData({ params, $axios }) {
-    const user = await $axios.$get(`/user/${params.id}`)
-    return { user }
+  data: () => ({
+    user: {
+      user_id: '',
+      account: {
+        sub: '',
+        created_at: '',
+        updated_at: ''
+      },
+      profile: {
+        profile_id: '',
+        icon_path: '',
+        name: '',
+        sex: 0,
+        birth_day: null,
+        school_year: null,
+        school_name: '',
+        profile: '',
+        using_os: '',
+        link: '',
+        contact: '',
+        created_at: '',
+        updated_at: ''
+      },
+      experiences: [],
+      products: [],
+      technologies: [],
+      is_active: true,
+      created_at: '',
+      updated_at: ''
+    }
+  }),
+  async created() {
+    const userId = this.$route.params.id
+    await this.$axios
+      .$get(`/user/${userId}`)
+      .then(response => {
+        this.user = response
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
