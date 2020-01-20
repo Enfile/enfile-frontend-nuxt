e2387539b876bd07c15b265dafc9aad3b767c546
agoetc <b6151@oic.jp>
2020-01-21 04:37:21 +0900

    fix lint

--- components/profile/Introduction.vue
+++ components/profile/Introduction.vue
@@ -17,8 +17,8 @@
         アルバイト
       </div>
       <div class="text--primary">
-        使用言語：C言語<br>
-        開発ジャンル：：ハード<br>
+        使用言語：C言語<br />
+        開発ジャンル：：ハード<br />
         2019/01 - 2019/10
       </div>
       <div class="text--primary">
@@ -28,8 +28,8 @@
         アルバイト
       </div>
       <div class="text--primary">
-        使用言語：C言語<br>
-        開発ジャンル：：ハード<br>
+        使用言語：C言語<br />
+        開発ジャンル：：ハード<br />
         2019/01 - 2019/10
       </div>
       <p class="display-1 text--primary">
@@ -42,8 +42,8 @@
         個人
       </div>
       <div class="text--primary">
-        使用言語：C言語<br>
-        開発ジャンル：：ハード<br>
+        使用言語：C言語<br />
+        開発ジャンル：：ハード<br />
         2019/01 - 2019/10
       </div>
       <div class="text--primary">
@@ -53,8 +53,8 @@
         チーム
       </div>
       <div class="text--primary">
-        使用言語：PHP<br>
-        開発ジャンル：：web<br>
+        使用言語：PHP<br />
+        開発ジャンル：：web<br />
         2019/01 - 2019/10
       </div>
       <p class="display-1 text--primary">
@@ -64,16 +64,16 @@
         C言語
       </div>
       <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
+        レベル：3<br />
+        期間：3年<br />
         何に使ったか：アルバイトで使用
       </div>
       <div class="text--primary">
         PHP
       </div>
       <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
+        レベル：3<br />
+        期間：3年<br />
         何に使ったか：個人での個人でのwebアプリ開発で使用
       </div>
       <p class="display-1 text--primary">
@@ -83,8 +83,8 @@
         C言語
       </div>
       <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
+        レベル：3<br />
+        期間：3年<br />
         何に使ったか：アルバイトで使用
       </div>
       <p class="display-1 text--primary">
@@ -94,22 +94,22 @@
         C言語
       </div>
       <div class="overline mb-4">
-        レベル：3<br>
-        期間：3年<br>
+        レベル：3<br />
+        期間：3年<br />
         何に使ったか：アルバイトで使用
       </div>
       <p class="display-1 text--primary">
         基本プロフィール
       </p>
       <div class="text--primary">
-        男性<br>
+        男性<br />
         生年月日：1996/01/21
       </div>
       <p class="display-1 text--primary">
         連絡先
       </p>
       <div class="text--primary">
-        ・https://twitter.com/xxxxxxx<br>
+        ・https://twitter.com/xxxxxxx<br />
         ・https://twitter.com/yyyyyyy
       </div>
     </v-card-text>
