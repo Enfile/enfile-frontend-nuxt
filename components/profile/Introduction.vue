432deb1db93b05849df4e7ea2b4dfe11501d7a67
小佐 海斗 <kosakaito@kosamac.local>
2020-01-21 03:17:48 +0900

    com 完了

--- components/profile/Introduction.vue
+++ components/profile/Introduction.vue
@@ -1,11 +1,116 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
+  <v-card class="mx-auto" max-width="auto">
     <v-card-text>
       <p class="display-1 text--primary">
         自己紹介
       </p>
       <div class="text--primary">
         hogehogehogehogehogehogehogehoeghhoehegheogheoheohehgegheoheheogh
+      </div>
+      <p class="display-1 text--primary">
+        業務経験
+      </p>
+      <div class="text--primary">
+        株式会社×××××
+      </div>
+      <div class="overline mb-4">
+        アルバイト
+      </div>
+      <div class="text--primary">
+        使用言語：C言語<br>
+        開発ジャンル：：ハード<br>
+        2019/01 - 2019/10
+      </div>
+      <div class="text--primary">
+        株式会社×××××
+      </div>
+      <div class="overline mb-4">
+        アルバイト
+      </div>
+      <div class="text--primary">
+        使用言語：C言語<br>
+        開発ジャンル：：ハード<br>
+        2019/01 - 2019/10
+      </div>
+      <p class="display-1 text--primary">
+        成果物
+      </p>
+      <div class="text--primary">
+        hogeシステム
+      </div>
+      <div class="overline mb-4">
+        個人
+      </div>
+      <div class="text--primary">
+        使用言語：C言語<br>
+        開発ジャンル：：ハード<br>
+        2019/01 - 2019/10
+      </div>
+      <div class="text--primary">
+        piyopiyo
+      </div>
+      <div class="overline mb-4">
+        チーム
+      </div>
+      <div class="text--primary">
+        使用言語：PHP<br>
+        開発ジャンル：：web<br>
+        2019/01 - 2019/10
+      </div>
+      <p class="display-1 text--primary">
+        使ったことある技術
+      </p>
+      <div class="text--primary">
+        C言語
+      </div>
+      <div class="overline mb-4">
+        レベル：3<br>
+        期間：3年<br>
+        何に使ったか：アルバイトで使用
+      </div>
+      <div class="text--primary">
+        PHP
+      </div>
+      <div class="overline mb-4">
+        レベル：3<br>
+        期間：3年<br>
+        何に使ったか：個人での個人でのwebアプリ開発で使用
+      </div>
+      <p class="display-1 text--primary">
+        使いたくない技術
+      </p>
+      <div class="text--primary">
+        C言語
+      </div>
+      <div class="overline mb-4">
+        レベル：3<br>
+        期間：3年<br>
+        何に使ったか：アルバイトで使用
+      </div>
+      <p class="display-1 text--primary">
+        使っているOS
+      </p>
+      <div class="text--primary">
+        C言語
+      </div>
+      <div class="overline mb-4">
+        レベル：3<br>
+        期間：3年<br>
+        何に使ったか：アルバイトで使用
+      </div>
+      <p class="display-1 text--primary">
+        基本プロフィール
+      </p>
+      <div class="text--primary">
+        男性<br>
+        生年月日：1996/01/21
+      </div>
+      <p class="display-1 text--primary">
+        連絡先
+      </p>
+      <div class="text--primary">
+        ・https://twitter.com/xxxxxxx<br>
+        ・https://twitter.com/yyyyyyy
       </div>
     </v-card-text>
   </v-card>
